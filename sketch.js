583c53125c178737901cc87af0d3acf4da4532ac
AnvayJain1745 <68576271+AnvayJain1745@users.noreply.github.com>
2020-08-17 17:45:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var bowl1,bowl2,bowl3;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	bowl1=createSprite(400,655,150,10);
+	bowl2=createSprite(325,635,10,50);
+	bowl3=createSprite(475,635,10,50);
+	bowl1.shapeColor="red";
+	bowl2.shapeColor="red";
+	bowl3.shapeColor="red";
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  canvas.depth=1;
+  helicopterSprite.depth=3;
+  packageSprite.depth=2;
+  packageSprite.bounceOff(bowl1);
+  drawSprites();
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody,false);
+	
+}
+}
